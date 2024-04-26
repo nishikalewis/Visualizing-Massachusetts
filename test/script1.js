7d38491fda7caec5ccc2ef3252f6b4b33266c3c5
nishikalewis <113398001+nishikalewis@users.noreply.github.com>
2024-04-25 23:32:26 -0400

    Delete test directory

--- test/script1.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    var header = document.querySelector('header');
-    if (window.scrollY > 50) { // Trigger change 50px from the top
-        header.classList.add('small-header');
-    } else {
-        header.classList.remove('small-header');
-    }
-});
